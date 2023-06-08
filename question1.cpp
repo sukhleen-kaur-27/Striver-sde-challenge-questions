759c80de2ea3a48b9bd263bd05915c69aa1f8b68
Sukhleen Kaur <74076814+sukhleen-kaur-27@users.noreply.github.com>
2023-06-08 13:56:16 +0530

    Create question1.cpp
    To set the elements of a row and column zero if it contains even a single zero.

---
+++ question1.cpp
@@ -0,0 +1,42 @@
+// set max array zero question
+// main function for input not created
+#include <bits/stdc++.h>
+
+void setZeros(vector<vector<int>> &matrix)
+{
+	// Write your code here.
+	int rows = 1;
+	int n = matrix.size();
+	int m = matrix[0].size();
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++){
+			if (matrix[i][j]==0){
+				matrix[i][0]=0;
+				if (j==0){
+					rows = 0;
+				}
+				else{
+				matrix[0][j]=0;
+				}
+			}
+		}
+	}
+	for(int i=1; i<n; i++){
+		for(int j=1; j<m; j++){
+			if (matrix[0][j]==0 || matrix[i][0]==0)
+				matrix[i][j]=0;
+			}
+	}
+
+	if (matrix[0][0]==0){
+		for(int j=0; j<m; j++){
+			matrix[0][j]=0;
+		}
+	}
+	if(rows==0){
+		for(int i=0;i<n;i++){
+			matrix[i][0]=0;
+		}
+	}
+
+}
